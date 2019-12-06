1d5f85de3eea7f69b91ae55e40f42f620d964a63
bgarrido7 <bianevesgarrido@gmail.com>
2019-12-06 11:17:58 +0000

    owner can see and add houses :tada:

--- Final Project/code/pages/houses.php
+++ Final Project/code/pages/houses.php
@@ -5,22 +5,23 @@ include_once('../database/houses.php');
 include_once('../database/user.php');
 
 echo nl2br("my houses \n");
-$row=0;
+
 //imprimir as casas deste owner
 $userID=getID($_SESSION['email']);
-
-if(getOwnerHouses($userID)!=null)
-    $row=(getOwnerHouses($userID)[0]['houseID']);
-
-if($row!=null)
-    getHouse($row);
+$housearray=getOwnerHouses($userID);
+foreach($housearray as $index){
+    getHouse($index['houseID']);
+}
 
 ?>
 </br>
 <a href="addHouse.php">
 
 <input type="button" value="Add New House"/>
+</a>
+
 <?php
+
 include_once("../template/common/footer.php");
 
 ?>
