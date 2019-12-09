ede124b5838db0bf1b5428a0e8447aa10d4a720b
bgarrido7 <bianevesgarrido@gmail.com>
2019-12-09 21:10:52 +0000

    code refactor all done

--- Final Project/code/pages/search.php
+++ Final Project/code/pages/search.php
@@ -7,30 +7,20 @@ include_once('../database/houses.php');
 
 foreach(getAllHouses() as $house){
 
-    echo nl2br ("\n\n---------------------------------------------------\nname: ");
-    echo $house['name'];
-    echo nl2br ("\nlocation: ");
-    echo $house['location'];
-    echo nl2br ("\nprice per day: ");
-    echo $house['dailyPrice'];
-    echo nl2br ("\ndescription: ");
-    echo $house['description'];
-    echo nl2br ("\npool: ");
-    echo $house['pool'];
-    echo nl2br ("\ncableTV: ");
-    echo $house['cableTV'];
-    echo nl2br ("\nWifi: ");
-    echo $house['Wifi'];
-    echo nl2br ("\nAC: ");
-    echo $house['AC'];
-    echo nl2br ("\nkitchen: ");
-    echo $house['kitchen'];
-    ?>
-    <form action="house.php" method="post" >
-    <input type="hidden" name="id" value="<?php echo $house['houseID']; ?>"/>
-    <input type="submit" value="see this house's page" />
-    </form>
-<?php
+    $houseID= $house['houseID'];
+    $houseName= $house['name'];
+    $local=$house['location'];
+    $price= $house['dailyPrice'];
+    $desc= $house['description'];
+    $pool= $house['pool'];
+    $cable= $house['cableTV'];
+    $wifi=$house['Wifi'];
+    $ac= $house['AC'];
+    $kitchen= $house['kitchen'];
+
+
+include("../template/content/list_all_houses.php");
+
 }
 
 
