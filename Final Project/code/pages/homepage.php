862507ca27b8a01b2441a4b732197761b3a36bcd
bgarrido7 <bianevesgarrido@gmail.com>
2019-12-11 12:08:37 +0000

    upload pic done :blush:

--- Final Project/code/pages/homepage.php
+++ Final Project/code/pages/homepage.php
@@ -4,8 +4,11 @@ include_once('../template/common/header.php');
 include_once('../database/user.php');
 
 $name=getName(getID($_SESSION['email'])['userID']);
+$userID= getID($_SESSION['email'])['userID'];
 
 include_once("../template/common/aside.php");
 include_once('../template/common/homepage.php');
 include_once("../template/common/footer.php");
 ?>
+
+
--- Final Project/code/template/common/homepage.php
+++ Final Project/code/template/common/homepage.php
@@ -5,6 +5,7 @@
 
 <h1> Welcome <?php echo htmlentities($name); ?> !<h1>
 
+<img src="../images/users/<?php echo htmlentities($userID);  ?>.jpg" alt="userPic" width="12%" height="25%">
 
 <h2> Go away this holiday season </h2>
 <h2> see our top pics: </h2>
