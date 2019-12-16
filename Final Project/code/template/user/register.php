dee2375252a80169a896111c10ff79f1c34bca9e
bgarrido7 <bianevesgarrido@gmail.com>
2019-12-16 18:16:08 +0000

    css almost done

--- Final Project/code/template/user/register.php
+++ Final Project/code/template/user/register.php
@@ -1,15 +1,14 @@
   <section id="register">
 
-   <h1>Register for an Account:</h1>
-   <form action="../actions/action_register.php" method="post" enctype="multipart/form-data" >
-    Username: <input type="text" name="name"  required="required"><br />
-    Email: <input type="email" name="email"  required="required"><br />
-    Password: <input type="password" name="pword" required="required"><br />
-    Repeat Password: <input type="password" name="repeatPword"  required="required"> <br/>
-    Birthday: <input type="date" value="2019-12-17" name="bday"><br/>
-    Profile Picture:<input type="file" name="fileToUpload" id="fileToUpload" >please only submit .jpg files <br/>    
-   <input type="submit" value="Register" />
-
+   <h1>Register for an Account:</h1> </br>
+   <form action="../actions/action_register.php" method="post" enctype="multipart/form-data" ></br>
+    Username: <input type="text" name="name"  required="required"><br /></br>
+    Email: <input type="email" name="email"  required="required"><br /></br>
+    Password: <input type="password" name="pword" required="required"><br /></br>
+    Repeat Password: <input type="password" name="repeatPword"  required="required"> </br><br/>
+    Birthday: <input type="date" value="2019-12-17" name="bday"><br/></br>
+    Profile Picture:<input type="file" name="fileToUpload" id="fileToUpload" >please only submit .jpg files <br/>    </br>   <input type="submit" value="Register" />
+    </br>
    <div id="error">
    <?php if(isset($_SESSION['error'])) echo htmlentities($_SESSION['error']); unset($_SESSION['error'])?>
 
